0abb07ae44e082ba2385d56d744d049127d1c303
Maksim Likhotin <777Max777@github.com>
2021-01-24 17:44:19 +0300

    fix cors policies

--- src/main/java/com/vorstu/excel/ExcelApplication.java
+++ src/main/java/com/vorstu/excel/ExcelApplication.java
@@ -2,6 +2,9 @@ package com.vorstu.excel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExcelApplication {
@@ -10,4 +13,14 @@ public class ExcelApplication {
         SpringApplication.run(ExcelApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
+    }
 }
